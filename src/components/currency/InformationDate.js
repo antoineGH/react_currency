7844e9bfff3bb3f3289022c1b2d4b9b956719152
Templars <antoine.ratat@gmail.com>
2021-03-27 15:40:17 +0800

    solve display issues

--- src/components/currency/InformationDate.js
+++ src/components/currency/InformationDate.js
@@ -6,14 +6,14 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 export default function InformationDate(props) {
-    if (props.state.date) {
-        return (
-            <Row>
-                <Col xs={12} sm={12} md={6}>
-                    {props.state.date && <p className='info_date'>{getDatetime(Date.parse(props.state.date))}</p>}
-                </Col>
-            </Row>
-        );
-    }
-    return null
+	if (props.state.date) {
+		return (
+			<Row className='row_info_date'>
+				<Col xs={12} sm={12} md={6} className='col_info_date'>
+					{props.state.date && <p>{getDatetime(Date.parse(props.state.date))}</p>}
+				</Col>
+			</Row>
+		)
+	}
+	return null
 }
